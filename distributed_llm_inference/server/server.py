4cc59aaa8a5a96db585d2a6cb266517d7bc1128b
Dylan Feng <dylan@sonnetai.com>
2024-08-02 02:22:42 -0700

    Initial code push

---
+++ distributed_llm_inference/server/server.py
@@ -0,0 +1,21 @@
+class Server:
+    def run(self):
+        while True:
+            # optimal block ids
+            # block_ids = self._get_blocks(...)
+            
+            # self.module = new Module(...)
+            
+            # update module code
+            try:
+                while True:
+                    # self.module.runtime.wait()
+                    # sleep for a random time
+                    # time.sleep(random.uniform(0, 1))
+
+                    # if not self.module.is_healthy(): break
+                    # if self.should_rebalance(): break
+                    break
+            finally:
+                # self.module.restart()
+                break
