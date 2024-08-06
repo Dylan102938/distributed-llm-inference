1ab5bce4282ff58d300db27655b058193667b764
Dylan Feng <dylan@sonnetai.com>
2024-08-05 18:13:58 -0700

    Adding inference decoder layer

--- distributed_llm_inference/models/llama.py
+++ distributed_llm_inference/models/llama.py
@@ -4,6 +4,9 @@ from typing import Optional, Tuple
 import torch
 import torch.nn.functional as F
 from transformers.models.llama.modeling_llama import (LlamaAttention,
+                                                      LlamaConfig,
+                                                      LlamaDecoderLayer,
+                                                      LlamaMLP, LlamaRMSNorm,
                                                       repeat_kv, rotate_half)
 
 from distributed_llm_inference.utils.cuda import \
@@ -35,7 +38,6 @@ class OptimizedLlamaInferenceAttention(LlamaAttention):
         attention_mask: Optional[torch.Tensor] = None,
         position_ids: Optional[torch.LongTensor] = None,
         past_key_value: Optional[Tuple[torch.Tensor]] = None,
-        output_attentions: bool = False,
     ):
         if self.config.pretraining_tp > 1:
             key_value_slicing = (self.num_key_value_heads * self.head_dim) // self.config.pretraining_tp
@@ -103,7 +105,7 @@ class OptimizedLlamaInferenceAttention(LlamaAttention):
                 f" {attn_output.size()}"
             )
 
-        attn_output = attn_output.transpose(1, 2).contiguous().reshape(bsz, q_len, -1)
+        attn_output = attn_output.transpose(1, 2).contiguous().reshape(bsz, q_len, self.hidden_size)
 
         if self.config.pretraining_tp > 1:
             attn_output = attn_output.split(self.hidden_size // self.config.pretraining_tp, dim=2)
@@ -113,3 +115,70 @@ class OptimizedLlamaInferenceAttention(LlamaAttention):
             attn_output = self.o_proj(attn_output)
         
         return attn_output, None, past_key_value
+    
+
+class OptimizedLlamaInferenceDecoderLayer(LlamaDecoderLayer):
+    def __init__(self, config: LlamaConfig):
+        torch.nn.Module.__init__(self)
+        self.hidden_size = config.hidden_size
+        self.self_attn = OptimizedLlamaInferenceAttention(config)
+        self.mlp = LlamaMLP(config)
+        self.input_layernorm = LlamaRMSNorm(self.hiden_size)
+        self.output_layernorm = LlamaRMSNorm(self.hidden_size)
+
+        self.pre_attn_graph = None
+        self.post_attn_graph = None
+    
+    def _optimized_input_norm(self, hidden_states):
+        if self.pre_attn_graph is None:
+            self.pre_attn_graph = make_inference_graphed_callable(
+                self.input_layernorm.forward, sample_args=(hidden_states,)
+            )
+        
+        return self.pre_attn_graph(hidden_states)
+
+    def _optimized_output_norm(self, hidden_states, attn_output):
+        if self.post_attn_graph is None:
+            self.post_attn_graph = make_inference_graphed_callable(
+                self.output_layernorm.forward, sample_args=(hidden_states, attn_output)
+            )
+        
+        return self.post_attn_graph(hidden_states, attn_output)
+    
+    def forward(
+        self,
+        hidden_states: torch.Tensor,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_value: Optional[Tuple[torch.Tensor]] = None,
+        **kwargs,
+    ) -> Tuple[torch.FloatTensor, Optional[Tuple[torch.FloatTensor, torch.FloatTensor]]]:
+        residual = hidden_states
+
+        bsz, q_len, _ = hidden_states.size()
+
+        if q_len == 1 and torch.is_inference_mode_enabled() and hidden_states.device.type == "cuda":
+            hidden_states = self._optimized_input_norm(hidden_states)
+        else:
+            hidden_states = self.input_layernorm(hidden_states)
+        
+        hidden_states, _, key_value = self.self_attn(
+            hidden_states,
+            attention_mask=attention_mask,
+            position_ids=position_ids,
+            past_key_value=past_key_value,
+            **kwargs
+        )
+
+        hidden_states = hidden_states + residual
+        residual = hidden_states
+
+        if q_len == 1 and torch.is_inference_mode_enabled() and hidden_states.device.type == "cuda":
+            hidden_states = self._optimized_output_norm(hidden_states, residual)
+        else:
+            hidden_states = self.output_layernorm(hidden_states + residual)
+        
+        hidden_states = self.mlp(hidden_states)
+        hidden_states = hidden_states + residual
+
+        return (hidden_states, key_value)
